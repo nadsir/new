69a8955b8053b2e60eb4f3fb4ccaac0b29f71f6b
98912 <nader.davarmanesh62@gmail.com>
2020-08-02 13:59:28 +0430

    this is the parallax effect with gsapTrigger

--- resources/js/gsap.js
+++ resources/js/gsap.js
@@ -2,9 +2,22 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".box", {
     scrollTrigger: {
         markers: true,
-        start: "top center",
-        scrub:true
+        trigger:".box",
+        scrub:2,
+        start: "top center ",
+        end:"top 100px"
+
 
 
     }, rotation: 90, x: 200, duration: 1
+});
+gsap.to(".flexBack2", {
+    yPercent: 50,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".flexBack2",
+        // start: "top bottom", // the default values
+        // end: "bottom top",
+        scrub: true
+    },
 });
